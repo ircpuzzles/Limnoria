6635a3542eb147482ee0e6cb9c3ed10837eef4b3
Matthias H <apoc@sixserv.org>
2014-04-09 00:43:44 +0200

    puzzle definitions: topic format strings

--- plugins/IrcPuzzles/local/challenge/game.py
+++ plugins/IrcPuzzles/local/challenge/game.py
@@ -22,90 +22,144 @@ class Game(object):
         self.puzzles = {}
         self.tracks = []
 
-        self.read_puzzles()
-        self.read_game()
+        self.name = None
 
-    def read_puzzles(self):
-        """Read all puzzle files for this game."""
+        self.begin= None
+        self.end = None
+
+        self.channel_prefix = None
+        self.tracks = []
+        self.lobby = None
+
+        self.parse_puzzles()
+        self.parse_game()
+
+    def parse_puzzles(self):
+        """Discover and parse puzzle for this game."""
         puzzle_path = join(self.path, 'puzzle')
         for filename in os.listdir(puzzle_path):
             path = join(puzzle_path, filename)
             if isfile(path) and filename.endswith('.json'):
-                puzzle = Puzzle(self, path)
+                puzzle = Puzzle.parse(path)
                 self.puzzles[puzzle.id] = puzzle
 
-    def read_game(self):
+    def parse_game(self):
         """Read game file."""
         logger.info('reading game definition file: %s', self.filename)
         with open(self.filename) as f:
-            parsed = json.load(f)
-        self.name = parsed['name']
-        # NOTE: make sure timezone information is maintained,
-        # 2008-08-12T12:20:30+0200
-        self.begin = dateutil.parser.parse(parsed['begin'])
-        self.end = dateutil.parser.parse(parsed['end'])
-        self.channel_prefix = parsed['channel_prefix']
-        for parsed_track in parsed['tracks']:
-            self.tracks.append(Track(self, parsed_track))
-        self.lobby = Channel(self.prefix_channel(parsed['lobby']['channel']),
-            parsed['lobby']['topic'])
+            content = json.load(f)
 
-    def prefix_channel(self, channel):
+        self.name = content['name']
+
+        # NOTE: make sure timezone information is maintained,
+        self.begin = dateutil.parser.parse(content['begin'])
+        self.end = dateutil.parser.parse(content['end'])
+
+        self.channel_prefix = content['channel_prefix']
+        for i,track in enumerate(content['tracks']):
+            self.tracks.append(Track.parse(self, track, i))
+        self.lobby = Channel(
+            self.format_channel(content['lobby']['channel']),
+            self.format_lobby_topic(content['lobby']['topic']))
+
+    def format_channel(self, channel):
         return self.channel_prefix + channel
 
+    def format_lobby_topic(self, topic_format):
+        keys = { 'name': self.name }
+        for i,track in enumerate(self.tracks):
+            keys.update({
+                ('track%d_name' % (i+1)): track.name,
+                ('track%d_start' % (i+1)): track.get_start().name
+            })
+
+        return topic_format % keys
+
 class Track(object):
-    def __init__(self, game, parsed_track):
-        self.name = parsed_track.get('name')
-        self.topic_format = parsed_track.get('topic_format')
-
-        puzzles = []
-        for puzzle_id in parsed_track['puzzles']:
-            if puzzle_id in game.puzzles:
-                puzzles.append(game.puzzles[puzzle_id])
-            else:
-                logger.warn('puzzle for track not found: %s', puzzle_id)
-
+    def __init__(self):
+        self.index = 0
+        self.name = None
+        # the topic format is applied to all channel topics, except the finish channel
+        self.topic_format = None
+        # Channel for this track, in order
         self.channel = []
+        self.channel_incorrect = []
+
+    @staticmethod
+    def parse(game, content, index):
+        track = Track()
+
+        track.index = index
+        track.name = content.get('name')
+        track.topic_format = content.get('topic_format')
+
+        puzzles = [game.puzzles[x] for x in content.get('puzzles', [])]
         for i in xrange(0, len(puzzles)+1):
-            # the channel name is the correct solution 
-            # of the previous level:
-            if i == 0: # start channel (without a solution to put in the name)
-                name = game.prefix_channel(parsed_track['start']['channel'])
+            puzzle = puzzles[i] if i<len(puzzles) else None
+
+            # channel name: correct solution of the previous level:
+            if i == 0: # start channel (without a previous puzzle)
+                name = game.format_channel(content['start']['channel'])
             else:
-                name = puzzles[i-1].get_correct_channel_name()
+                name = game.format_channel(puzzles[i-1].get_correct())
 
-            if i < len(puzzles):
-                topic = puzzles[i].get_topic()
+            # channel topic: current puzzle clue
+            if not puzzle: # finish channel (without current puzzle)
+                topic = content['finish']['topic']
             else:
-                topic = parsed_track['finish']['topic']
+                topic = track.format_puzzle_topic(puzzles[i], game, i+1)
 
-            puzzle_channel = Channel(name, topic)
+            channel = Channel(name, topic)
 
-            # set cross references to puzzle instances:
-            puzzle_channel.name_puzzle = puzzles[i-1] if i > 0 else None
-            puzzle_channel.topic_puzzle = puzzles[i] if i < len(puzzles) else None
+            # set cross reference to puzzle:
+            channel.puzzle = puzzle
 
             # set incorrect channel list:
-            if i < len(puzzles):
-                puzzle_channel.incorrect_next = puzzles[i].get_incorrect_channel()
+            if puzzle:
+                for incorrect in puzzle.get_incorrect():
+                    incorrect_channel = Channel(
+                        incorrect,
+                        puzzle.incorrect_topic
+                    )
+                    puzzle.next_incorrect.append(incorrect_channel)
+                    # also generate a per-track list of incorrect channel:
+                    track.channel_incorrect.append(incorrect_channel)
 
-            self.channel.append(puzzle_channel)
+            track.channel.append(channel)
 
-        # double link the channel internally:
-        for i in xrange(0, len(self.channel)):
-            self.channel[i].prev = self.channel[i-1] if i > 0 else None
-            self.channel[i].next = self.channel[i+1] if i < len(self.channel)-1 else None
+        # build internal links between channel:
+        for i in xrange(0, len(track.channel)):
+            track.channel[i].prev = track.channel[i-1] if i > 0 else None
+            track.channel[i].next = track.channel[i+1] if i < len(track.channel)-1 else None
 
-        # generate channel for incorrect solutions:
-        self.incorrect_channel = []
-        for puzzle in puzzles:
-            self.incorrect_channel += puzzle.get_incorrect_channel()
+        return track
 
     def get_start(self):
         return self.channel[0]
 
     def get_finish(self):
         return self.channel[-1]
+
+    def format_puzzle_topic(self, puzzle, game, puzzle_num):
+        """Format the topic, the following format keys are placed:
+
+            %(clue)s - the clue of the puzzle
+            %(hint)s - the first hint of the puzzle
+            %(track_name)s - name of the track
+            %(track_num)s - number of the track (starting with 1)
+            %(puzzle_num)s - number of the puzzle in current track
+            %(puzzle_max)s - puzzle count in this track
+            %(game_name)s - puzzle count in this track
+        """
+        return self.topic_format % {
+            'clue': puzzle.clue,
+            'hint': puzzle.hints[0] if len(puzzle.hints) else '-',
+            'track_name': self.name,
+            'track_num': self.index+1,
+            'puzzle_num': puzzle_num,
+            'puzzle_max': len(self.channel)-1,
+            'game_name': game.name
+        }
 
 if __name__ == '__main__':
     logger.addHandler(logging.StreamHandler())
@@ -123,17 +177,19 @@ if __name__ == '__main__':
     first_track = game.tracks[0]
     print len(first_track.channel) #> list of all (correct) channels in this track
 
+    print game.lobby
+
     # get tracks:
     for chan in game.tracks[0].channel:
         print '%s || %s' % (chan.name, chan.topic)
     print
-    for chan in game.tracks[0].incorrect_channel:
+    for chan in game.tracks[0].channel_incorrect:
         print '%s || %s' % (chan.name, chan.topic)
     print
     for chan in game.tracks[1].channel:
         print '%s || %s' % (chan.name, chan.topic)
     print
-    for chan in game.tracks[1].incorrect_channel:
+    for chan in game.tracks[1].channel_incorrect:
         print '%s || %s' % (chan.name, chan.topic)
 
     # get lobby channel
--- plugins/IrcPuzzles/local/challenge/puzzle.py
+++ plugins/IrcPuzzles/local/challenge/puzzle.py
@@ -1,47 +1,65 @@
-
 import os
-from os.path import join, isfile
+from os.path import basename, splitext
 import json
 import logging
 logger = logging.getLogger('ircpuzzles')
 
-import dateutil.parser # pip install dateutil
-
 from channel import Channel
 
 class Puzzle(object):
-    def __init__(self, game, filename):
-        """Load puzzle data.
+    def __init__(self, id):
+        """Initialize puzzle definition."""
+        self.id = id
+        self.name = None
+        self.creator = None
+        self.clue = None
+        self.hints = None
+        self.incorrect_topic = None
+        self.solution = None
+
+        self.prev = None
+        self.next = None
+        self.next_incorrect = []
+        self.puzzle = None
+
+    def get_correct(self):
+        return self.solution.get('correct')
+
+    def get_incorrect(self):
+        return self.solution.get('incorrect', [])
+
+    @staticmethod
+    def parse(path):
+        """Load specified puzzle json definition.
 
         Args:
-           filename (str): Path to puzzle json file.
+            filename (str): path to puzzle json file.
         """
-        self.game = game
-        self.id = os.path.basename(filename)[0:-5]
+        id, ext = splitext(basename(path))
 
-        logger.info('load puzzle definition file: %s', filename)
-        with open(filename) as f:
-            parsed = json.load(f)
-        self.name = parsed['name']
-        self.creator = parsed['creator']
-        self.clue = parsed['clue']
-        self.incorrect_topic = parsed['incorrect_topic'] if 'incorrect_topic' in parsed else ''
-        self.hints = parsed['hints'] if 'hints' in parsed else []
-        solution = parsed['solution']
-        self.correct_solution = solution['correct']
-        self.incorrect_solutions = solution['incorrect'] if 'incorrect' in solution else []
+        logger.info('reading puzzle definition file: %s', path)
+        with open(path) as f:
+            content = json.load(f)
+            puzzle = Puzzle(id)
+            puzzle.name = content.get('name')
+            puzzle.creator = content.get('creator')
+            puzzle.clue = content.get('clue')
+            puzzle.hints = content.get('hints', [])
+            puzzle.incorrect_topic = content.get('incorrect_topic', '')
+            puzzle.solution = content.get('solution')
+            return puzzle
 
-    def get_topic(self):
-        return self.clue
+    #def get_topic(self):
+    #    return self.clue
 
-    def get_correct_channel_name(self):
-        return self.game.prefix_channel(self.correct_solution)
+    #def get_correct_channel_name(self):
+    #    return self.game.format_channel(self.correct_solution)
 
-    def get_incorrect_channel(self):
-        incorrect_channel = []
-        for name in self.incorrect_solutions:
-            channel = Channel(self.game.prefix_channel(name), self.incorrect_topic)
-            channel.name_puzzle = self
-            incorrect_channel.append(channel)
-        return incorrect_channel
+    #def get_incorrect_channel(self):
+    #    incorrect_channel = []
+    #    for name in self.incorrect_solutions:
+    #        channel = Channel(self.game.format_channel(name), self.incorrect_topic)
+    #        channel.name_puzzle = self
+    #        incorrect_channel.append(channel)
+    #    return incorrect_channel
 
